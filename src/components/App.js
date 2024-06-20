e8851ab68ed5619a12799d5501438359d73f7a7a
Liam Simmons <liam.simmons@powerhrg.com>
2024-06-20 14:12:53 -0400

    Refactor for class components

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,26 @@
-import React from "react";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import About from "./About";
+import React from "react"
+import NavBar from "./NavBar"
+import Home from "./Home"
+import About from "./About"
 
 // pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
+import user from "../data/user"
+console.log(user)
 
-function App() {
-  return (
-    <div>
-      <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <Home
+          username={this.user.name}
+          city={this.user.city}
+          color={this.user.color}
+        />
+        <About bio={this.user.bio} links={this.user.links} />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
